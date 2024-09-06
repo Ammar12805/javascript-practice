94507b24a23c126ffd24571c4c2d1ebe483304cd
abeer-mazher <abeermazher10@gmail.com>
2024-09-06 15:42:27 +0500

    add files

---
+++ app.js
@@ -0,0 +1,222 @@
+// CHAPTER 1(alert)
+// Q1
+
+// alert("Hello! Welcome To Our Page")
+
+// Q2
+
+// alert("Error! Please enter a valid password")
+
+// Q3
+
+// alert("Welcome to JS Land.. \n Happy Coding!")
+
+
+// Q4
+
+// alert("Welcome to JS Land..")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs")
+
+
+// Q5
+
+// alert("Hello... I can run JS through my web borwser's console")
+
+
+// Q6
+
+// alert("Here u Go")
+
+
+// Chapter 2(Variable for string)
+
+// Q1
+
+// var username;
+
+// Q2
+
+// var myName = "Ammar Ahmed"
+
+// Q3
+
+// var message ="Hello World"
+
+// alert(message)
+
+// Q4
+
+// var name = "Jhone Doe";
+// var age = 15;
+// var skill ="Certified Mobile Application Developent";
+
+// alert(name);
+// alert(age);
+// alert(skill);
+
+// Q5
+
+// var a = "Pizza \n Pizz \n Piz \n Pi \n P"
+
+// alert(a)
+
+// Q6
+
+// var email = "example@example.com"
+// alert(email)
+
+// Q7
+
+// var book = "I am learning JS from . A smarter way to Learn JavaScript"
+// alert(book)
+
+// Q8
+
+// var script = "Yah! I can write HTML content through JavaScript"
+// document.write(script)
+
+// Q9
+
+// var app = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(app)
+// document.write(app)
+
+
+// Chapter 3(Variable for Numbers)
+
+// Q1
+
+// var age = 19;
+// alert("I am "+ age + " years old")
+
+// Q2
+
+// var visit = 14;
+
+// alert("You have visited this site "+ visit + " times")
+
+// Q3
+
+// var birthYear = 2005;
+// document.write("My birth year is "+ birthYear +  " <br> Data type of my declared variable is number ")
+
+// Q4
+
+// var visitorName = "John Doe";
+// var productTitle = "T-Shirt";
+// var quantity = 10 ;
+//  document.write(visitorName + " ordered "+ quantity + " " + productTitle + " on XYZ Clothing store" )
+
+// Chapter 4 (Variable name LEGAL & ILLEGAL)
+
+// Q1
+//  var num1;
+//  var num2;
+//  var num3;
+//  console.log(num1 + num2 + num3)
+
+// var num1 ,num2,num3
+// Q2
+
+// ILLEGAL
+// var 2345;
+// var 237num;
+// var @num;
+// var first name;
+// var num%;
+
+
+// LEGAL
+// var firstName;
+// var last_name;
+// var num;
+// var $advantage;
+// var go$12;
+
+// Q3
+
+// var a = "$" , b = "my" , c = "_1st" , d = "variable" , e = "$name" , f ="_name" , g = "name" , h ="sensitive" , i = 'keywords'
+
+// document.write("<h1>Rules of Naming JS Variable</h1> <br> <br> Variable names can only contain "+ a +", "+ b + " , "+ c + " and " + d + " <br> Variable must begin with a "+ e +","+ f + " or " + g + "<br> Variable names are case " + h + "<br> Variable names should not be JS " + i)
+
+
+// Chapter 1-3 docs 
+
+// chapter 1 (alert)
+
+// Q1
+
+// alert("Ammar")
+// alert("Ahmed")
+// alert("ammarahmed12805@gmail.com")
+// alert("03022215858")
+// alert("Password")
+
+// Q2
+// Correct this statement: alert &quot;You&#39;re learning JavaScript!&quot; ;
+
+// alert("You're learning JavaScript !")
+
+// Q3
+ 
+// alert("Hello Dear")
+
+// Chapter 2 (Variable for strings)
+
+// Q1
+
+// var firstName;
+
+// Q2
+
+// var name;
+
+// name = "Ammar";
+
+// Q3
+
+    // var teamName = "Real Madrid"
+
+    // alert(teamName)
+
+    // Q4
+
+    // var bestMan = "Charlie";
+    // bestMan = "John Doe"
+
+    // Chapter 3 (Variable for Numbers)
+
+    // Q1
+
+    // var caseQty;
+
+    // Q2
+   
+    // var caseQty = 50;
+    // caseQty = 144;
+
+    //   Q3
+
+    // var num = "9";
+    //  var num = 9 ;
+
+    //  Q4
+
+    // var num1 = 50 , num2 = 50
+
+    // console.log(num1 + num2)
+
+    // Q5
+
+//     var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// console.log(orderTotal)
+
+// Q6
+
+// var num = 20;
+// var total = num + 50;
+
+// console.log(total)
